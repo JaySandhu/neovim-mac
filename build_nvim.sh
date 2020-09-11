3d58769da436c7261d000229be428087f53492d2
Jay Sandhu <jaysandhu1993@gmail.com>
2020-09-11 16:01:55 +0100

    Rework build script to account for failed builds
     - Always run make. The makefile will avoid rebuilding when it can, so
     this won't lead to pointless rebuilds. Previously we skipped the make
     step if a neovim directory was detected, this wasn't a reliable test as
     the neovim directory could be left over from a failed build.
     - Do not explicitly build neovim dependencies. The makefile will take
     care of it for us.
     - Add error checking
    
    Closes #2.

--- build_nvim.sh
+++ build_nvim.sh
@@ -25,24 +25,20 @@
 #   build/nvim/lib
 #   build/nvim/share
 
+set -e
+
 pushd "$(dirname "$0")"
 mkdir -p build
 cd build
 
 if [ ! -d "neovim" ]; then
     git clone --depth 1 "https://github.com/JaySandhu/neovim.git" -b "release-0.4-patched"
-    cd neovim
-
-    mkdir .deps
-    cd .deps
-    cmake ../third-party
-    make
-    cd ..
-
-    make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=.."
-    make install
-    cd ..
 fi
+
+cd neovim
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=.."
+make install
+cd ..
 
 echo "Making directories"
 rm -rf nvim
