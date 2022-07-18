b63c59021409b41126cd121a9f9d404f2fc0b625
Jay Sandhu <jaysandhu1993@gmail.com>
2022-07-18 17:31:40 +0100

    Replace deprecated recordFailure method
    Switch to the new issue based API.

--- test/DeathTest.m
+++ test/DeathTest.m
@@ -24,14 +24,34 @@ static inline bool try_waitpid(pid_t pid, int *status) {
     }
 }
 
+static bool report_issue(XCTestCase *test, NSString *description,
+                         NSString *user_message, NSString *filename, NSUInteger line) {
+    if ([user_message length]) {
+        description = [NSString stringWithFormat:@"%@ - %@", description, user_message];
+    }
+
+    XCTSourceCodeLocation *location = [[XCTSourceCodeLocation alloc] initWithFilePath:filename lineNumber:line];
+    XCTSourceCodeContext *sourceCodeContext = [[XCTSourceCodeContext alloc] initWithLocation:location];
+
+    XCTIssue *issue = [[XCTIssue alloc] initWithType:XCTIssueTypeAssertionFailure
+                                  compactDescription:description
+                                 detailedDescription:nil
+                                   sourceCodeContext:sourceCodeContext
+                                     associatedError:nil
+                                         attachments:@[]];
+
+    [test recordIssue:issue];
+    return false;
+}
+
 bool forked_context(XCTestCase *test, int testcode,
                     bool equal, const char *assertion,
                     const char *failure_message, NSString *user_message,
                     NSString *filename, NSUInteger line) {
     pid_t pid = fork();
-    
+
+    // Child process, supress stderr and set death callbacks
     if (pid == 0) {
-        // Child process, supress stderr and set death callbacks
         dup2(open("/dev/null", O_WRONLY), STDERR_FILENO);
 
 #if __has_feature(address_sanitizer)
@@ -41,33 +61,23 @@ bool forked_context(XCTestCase *test, int testcode,
         signal(SIGABRT, _exit);
         return true;
     }
-    
+
     int status;
-    BOOL expexted_failure;
-    NSString *description;
-    
+
     if (pid == -1) {
-        expexted_failure = NO;
-        description = [NSString stringWithFormat:@"%s failed: fork error: %s", assertion, strerror(errno)];
-    } else if(!try_waitpid(pid, &status)) {
-        expexted_failure = NO;
-        description = [NSString stringWithFormat:@"%s failed: waitpid error: %s", assertion, strerror(errno)];
-    } else if ((WEXITSTATUS(status) == testcode) != equal) {
-        expexted_failure = YES;
-        description = [NSString stringWithFormat:@"%s failed: %s", assertion, failure_message];
-    } else {
-        // Test passed
-        return false;
+        NSString *description = [NSString stringWithFormat:@"%s failed: fork error: %s", assertion, strerror(errno)];
+        return report_issue(test, description, user_message, filename, line);
     }
-    
-    if ([user_message length]) {
-        description = [NSString stringWithFormat:@"%@ - %@", description, user_message];
+
+    if(!try_waitpid(pid, &status)) {
+        NSString *description = [NSString stringWithFormat:@"%s failed: waitpid error: %s", assertion, strerror(errno)];
+        return report_issue(test, description, user_message, filename, line);
     }
-    
-    [test recordFailureWithDescription:description
-                                inFile:filename
-                                atLine:line
-                              expected:expexted_failure];
-    
+
+    if ((WEXITSTATUS(status) == testcode) != equal) {
+        NSString *description = [NSString stringWithFormat:@"%s failed: %s", assertion, failure_message];
+        return report_issue(test, description, user_message, filename, line);
+    }
+
     return false;
 }
