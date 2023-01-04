0b8290ab43d2deb7aa4964eb8716f3829d780aa7
Jay Sandhu <jaysandhu1993@gmail.com>
2023-01-04 15:38:32 +0000

    Add version macros

---
+++ src/version.h
@@ -0,0 +1,16 @@
+//
+//  Neovim Mac
+//  version.h
+//
+//  Copyright © 2023 Jay Sandhu. All rights reserved.
+//  This file is distributed under the MIT License.
+//  See LICENSE.txt for details.
+//
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define NEOVIM_MAC_MAJOR_VERSION 0
+#define NEOVIM_MAC_MINOR_VERSION 2
+
+#endif // VERSION_H
