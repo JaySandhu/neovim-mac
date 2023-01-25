117cf39be9f50f37ea32b2f07271d667ab979d75
Jay <jaysandhu1993@gmail.com>
2023-01-25 19:03:49 +0000

    Revert "Title bar drags don't call nvim.input_mouse() (#15)" (#17)
    This reverts commit 352a9277fb0b03eb36a161ecff888341e3ee038c.

--- src/NVWindowController.mm
+++ src/NVWindowController.mm
@@ -804,9 +804,6 @@ static inline bool pointInGrid(nvim::grid_point point, nvim::grid_size size) {
     }
 
     NSPoint windowLocation = [event locationInWindow];
-    if (!NSPointInRect(windowLocation, self.window.contentLayoutRect)) {
-        return;
-    }
     nvim::grid_point location = [gridView cellLocation:windowLocation clampTo:lastGridSize];
     nvim::grid_point &lastLocation = lastMouseLocation[button];
 
